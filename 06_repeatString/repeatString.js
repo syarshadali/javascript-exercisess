426ff099f4227d5f4aa55606bc050bdfc1a5f16a
Syed Arshad Ali <ssyarshadali@gmail.com>
2026-04-07 09:12:07 +0500

    Add code to repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,13 +1,11 @@
 const repeatString = function(string, num) {
-    if (num < 0) console.log("ERROR");
+    if (num < 0) return "ERROR";
     let str = "";
     for (let i = 0; i < num; i++) {
         str += string;
     }
-    console.log(str);
+    return str;
 };
-
-repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
