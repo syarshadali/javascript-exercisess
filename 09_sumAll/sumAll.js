f57964fbbe5b3e07743445d04d9c726500d86468
Syed Arshad Ali <ssyarshadali@gmail.com>
2026-05-20 11:35:02 +0500

    Add code to sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(prev, curr) {
+    if (!Number.isInteger(prev) || !Number.isInteger(curr)) return "ERROR";
+    if (prev < 0 || curr < 0) return "ERROR";
+    if (prev > curr) {
+        const temp = prev;
+        prev = curr;
+        curr = temp;
+    }
 
+    let sum = 0;
+    for (let i = prev; i <= curr; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
